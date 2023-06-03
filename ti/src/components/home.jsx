b444dc07df418044a87c8e9e50d3f796905fad9b
Bryaan159 <00057720@uca.edu.sv>
2023-06-03 02:22:47 -0600

    Se arreglo el menu navegable :fire:

--- ti/src/components/home.jsx
+++ ti/src/components/home.jsx
@@ -2,28 +2,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
 import "./home.css"
+
 const Home = () => {
     return (
-        <div className='header'>
-            <div className='logo'>LOGO</div>
-            <button className='abrir-menu'>Abrir</button>
-            <nav className='nav'>
-                <button>Cerrar</button>
-                <ul className='nav-list'>
-                    <li><a href="#">All</a></li>
-                    <li><a href="#">New</a></li>
-                    <li><a href="#">Cultural</a></li>
-                    <li><a href="#">Artistic</a></li>
-                    <li>
-                        <a href="#">
-                            <FontAwesomeIcon icon={faSearch} />
-                        </a>
-                    </li>
-                    <li><a href="#">
-                        <FontAwesomeIcon icon={faCartArrowDown} />
-                    </a></li>
-                </ul>
-            </nav>
+        <div>
+            <header className='header'>
+                <div className='logo'>LOGO</div>
+                <nav className='nav'>
+                    <ul className='nav-list'>
+                        <li><a href="#">All</a></li>
+                        <li><a href="#">New</a></li>
+                        <li><a href="#">Cultural</a></li>
+                        <li><a href="#">Artistic</a></li>
+                        <li>
+                            <a href="#">
+                                <FontAwesomeIcon icon={faSearch} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <FontAwesomeIcon icon={faCartArrowDown} />
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         </div>
     );
 }
