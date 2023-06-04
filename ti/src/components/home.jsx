81faedfdb17001c8c6a7ede8e30e502396a06467
Bryaan159 <00057720@uca.edu.sv>
2023-06-03 18:31:54 -0600

    Se arreglo correctamente el menu en Home.jsx

--- ti/src/components/home.jsx
+++ ti/src/components/home.jsx
@@ -1,27 +1,33 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import "./home.css"
 
 const Home = () => {
     return (
         <div>
             <header className='header'>
-                <div className='logo'>LOGO</div>
+                <div className='logo'><a href="#">LOGO</a></div>
                 <nav className='nav'>
                     <ul className='nav-list'>
-                        <li><a href="#">All</a></li>
-                        <li><a href="#">New</a></li>
-                        <li><a href="#">Cultural</a></li>
-                        <li><a href="#">Artistic</a></li>
-                        <li>
+                        <li className='all'><a href="#">All</a></li>
+                        <li className='new'><a href="#">New</a></li>
+                        <li className='cultural'><a href="#">Cultural</a></li>
+                        <li className='artistic'><a href="#">Artistic</a></li>
+                        <li className='searchI'>
                             <a href="#">
                                 <FontAwesomeIcon icon={faSearch} />
                             </a>
                         </li>
-                        <li>
+                        <li className='cartI'>
                             <a href="#">
                                 <FontAwesomeIcon icon={faCartArrowDown} />
+                            </a>
+                        </li>
+                        <li className='barsI'>
+                            <a href="#">
+                                <FontAwesomeIcon icon={faBars} />
                             </a>
                         </li>
                     </ul>
